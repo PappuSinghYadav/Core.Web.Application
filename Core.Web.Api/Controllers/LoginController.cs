5c341de56aa38f0c8b4e24bbd36e426455aa4249
Pappu Singh <pappu.s@omnibiz.com>
2024-06-22 11:57:36 +0530

    Added sql script and controller

---
+++ Core.Web.Api/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Web.Http;
+
+namespace Core.Web.Api.Controllers
+{
+    [System.Web.Http.Authorize]
+    [ApiController]
+    [System.Web.Http.Route("api/[controller]")]
+    public class LoginController : ApiController
+    {
+        IMapper _mapper;
+        public LoginController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+    }
+}
