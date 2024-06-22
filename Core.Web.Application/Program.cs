49ae78f2fab3ba468ffd92ce8a41e544691b5beb
Pappu Singh <pappu.s@omnibiz.com>
2024-06-22 10:58:17 +0530

    Added new anargan program

--- Core.Web.Application/Program.cs
+++ Core.Web.Application/Program.cs
@@ -1,3 +1,8 @@
+using Newtonsoft.Json;
+using System.Data.SqlClient;
+using System.Net.Http.Headers;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -13,6 +18,12 @@ builder.Services.AddCors(options =>
                 .AllowAnyMethod();
     });
 });
+
+builder.Services.AddMvc(a =>
+{
+    a.RespectBrowserAcceptHeader = true;
+    a.ReturnHttpNotAcceptable = true;
+}).AddXmlDataContractSerializerFormatters();
 
 var app = builder.Build();
 
@@ -37,3 +48,85 @@ app.MapControllerRoute(
 app.MapFallbackToFile("index.html");
 
 app.Run();
+
+
+
+
+
+
+
+
+
+using (var client = new HttpClient())
+{
+    client.BaseAddress = new Uri("API Base URL");
+    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("");
+    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+    var result = await client.DeleteAsync("/");
+
+    var result1 = await client.GetAsync("/");
+
+    var content = new StringContent(JsonConvert.SerializeObject("content"));
+
+    var result2 = await client.PostAsync("/", content);
+
+    bool output = false;
+    if(result.IsSuccessStatusCode)
+    {
+        output = true;
+    }
+    else
+    {
+        output = false;
+    }
+}
+
+
+
+List<Thread> threads = new List<Thread>();
+for (int i = 0; i < 10; i++)
+{
+    int temp = i;
+    Thread t = new Thread(() => Console.WriteLine(temp));
+    t.Start();
+    threads.Add(t);
+}
+
+foreach (Thread t in threads)
+{
+    t.Join(); // Used to wait till thread ends
+}
+
+//Note: this supports in dotnet 3.5 version
+
+Task task = Task.Run(() => Console.WriteLine());
+
+Task task1 = Task.Factory.StartNew(() => Console.WriteLine());
+
+Task task2 = Task.Factory.StartNew(() => Console.WriteLine());
+
+Task.WaitAll(task,task1,task2);
+
+
+
+
+var conn1 = app.Configuration.GetConnectionString("DefaultConnection");
+//OR
+var conn2 = app.Configuration["ConnectionStrings:DefaultConnection"];
+
+using(SqlConnection scon = new SqlConnection(conn1))
+{
+    scon.Open();
+    using(SqlCommand cmd = scon.CreateCommand())
+    {
+        var rdr = cmd.ExecuteReader();
+        while (rdr.Read())
+        {
+            Console.WriteLine(rdr.GetString(0));
+        }
+    }
+    scon.Close();
+}
+
+//Note: This will work without docker only
--- PracticeCSharpProgram/ProgramLists.cs
+++ PracticeCSharpProgram/ProgramLists.cs
@@ -8,11 +8,26 @@ namespace PracticeCSharpProgram
 {
     public class BalancedParentheses
     {
-        public static bool IsBalanced(string str)
+        public static bool IsBalanced()
         {
-            string str1 = "()";
-            string str2 = "([]{})";
-            string str3 = "([)]";
+            string str1 = "([)]";
+            //anargan
+            string strs = "15651";
+            for (int i = 0; i < strs.Length; i++)
+            {
+                if (strs[i] == strs[strs.Length - 1])
+                {
+                    if(strs.Length - 1 == i)
+                        return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+
+            bool Output = false;
             Stack<char> endings = new Stack<char>();
             foreach (var curr in str1)
             {
@@ -29,15 +44,71 @@ namespace PracticeCSharpProgram
                         break;
                     default:
                         if (endings.Count != 0 && endings.Pop() == curr)
-                            return true;
+                        {
+                            Output = true; break;
+                        }
                         else
-                            return false;
+                            Output = false; break;
 
                 }
             }
+             
+
             return true;
         }
 
         
     }
 }
+
+
+
+//string str = "hello world";
+
+//string Response = string.Empty;
+//string word = string.Empty;
+//foreach (char a in str)
+//{
+//    if (a == ' ')
+//    {
+//        Response = Response + " " + word;
+//        word = string.Empty;
+//    }
+//    else
+//    {
+//        word = a + word;
+//    }
+//}
+//Response = Response + " " + word;
+
+//var result = Response;
+
+
+//int[] a = { 1, 2, 3 };
+
+//int[,] b = { { 1, 2, 3 }, { 4, 5, 6 } };
+
+//int[,,] c = {
+//                    {
+//                      {  1 ,  2, } ,
+//                      {  3 ,  4 , } ,
+//                      {  5 , 6 , } ,
+//                    } ,
+//                    {
+//                      { 7 , 8 , } ,
+//                      { 9 , 10 , } ,
+//                      { 11 , 12 , } ,
+//                    } ,
+//                  };
+
+
+//for (int i = 0; i < c.GetLength(0); i++)
+//{
+//    for (int j = 0; j < c.GetLength(1); j++)
+//    {
+//        for (int k = 0; k < c.GetLength(2); k++)
+//        {
+//            Console.WriteLine(c[i, j, k]);
+//        }
+//    }
+//}
